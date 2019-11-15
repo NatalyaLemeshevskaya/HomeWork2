3183a60e61afd8b8e48fa5c162d2221fb30889e4
natallialemiasheuskaya <natkaber1995@gmail.com>
2019-11-15 22:27:24 +0300

    first part + keyboard

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -8,13 +8,104 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
+    @IBOutlet var colorView: UIView!
+    
+
+    let minValueSlider:Float = 0.0
+    let maxValueSlider:Float = 1.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.minimumValue = minValueSlider
+        redSlider.maximumValue = maxValueSlider
+        redSlider.minimumTrackTintColor = .red
+        
+        greenSlider.minimumValue = minValueSlider
+        greenSlider.maximumValue = maxValueSlider
+        greenSlider.minimumTrackTintColor = .green
+        
+        blueSlider.minimumValue = minValueSlider
+        blueSlider.maximumValue = maxValueSlider
+        blueSlider.minimumTrackTintColor = .blue
+        
+        changeColor()
+        changeValue(label: redLabel, textField: redTextField, slider: redSlider)
+        changeValue(label: greenLabel, textField: greenTextField, slider: greenSlider)
+        changeValue(label: blueLabel, textField: blueTextField, slider: blueSlider)
+        
+        redTextField.keyboardType = UIKeyboardType.decimalPad
+        greenTextField.keyboardType = UIKeyboardType.decimalPad
+        blueTextField.keyboardType = UIKeyboardType.decimalPad
+        
+        setDoneOnKeyboard(textFieldRed: redTextField, textFieldGreen: greenTextField, textFieldBlue: blueTextField)
+    
     }
 
+    
+    private func changeValue(label: UILabel, textField: UITextField, slider: UISlider) {
+        label.text = String(format: "%.2f", slider.value)
+        textField.text = String(format: "%.2f", slider.value)
+        
+    }
+    
+    private func changeColor()  {
+
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+
+    @IBAction func redSliderAction() {
+        changeColor()
+        changeValue(label: redLabel, textField: redTextField, slider: redSlider)
+    }
+    
+    @IBAction func greenSliderAction() {
+        changeColor()
+        changeValue(label: greenLabel, textField: greenTextField, slider: greenSlider)
+    }
+    
+    @IBAction func blueSliderAction() {
+        changeColor()
+        changeValue(label: blueLabel, textField: blueTextField, slider: blueSlider)
+    }
 
 }
+
+extension UIViewController: UITextFieldDelegate {
+    
+    func setDoneOnKeyboard(textFieldRed: UITextField, textFieldGreen: UITextField, textFieldBlue: UITextField) {
+    let keyboardToolbar = UIToolbar()
+    keyboardToolbar.sizeToFit()
+    let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    let doneBarButton = UIBarButtonItem(barButtonSystemItem: .done, target: self,
+                                        action: #selector(dismissKeyboard))
+    keyboardToolbar.items = [flexBarButton, doneBarButton]
+        
+    textFieldRed.inputAccessoryView = keyboardToolbar
+    textFieldGreen.inputAccessoryView = keyboardToolbar
+    textFieldBlue.inputAccessoryView = keyboardToolbar
+
+}
+
+    @objc func dismissKeyboard() {
+    view.endEditing(true)
+ }
+
+}
+
+
 
